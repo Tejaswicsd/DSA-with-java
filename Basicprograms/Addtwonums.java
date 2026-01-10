25b7c2752279378047a4a5492202f945f212d742
Tejaswicsd <tejaswiguttula1@gmail.com>
2026-01-10 23:03:03 +0530

    add Addtwonums program to calculate the sum of two numbers

---
+++ Basicprograms/Addtwonums.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Addtwonums{
+public static void main(String[] args){
+Scanner in=new Scanner(System.in);
+int n=in.nextInt();
+int m=in.nextInt();
+int sum=n+m;
+System.out.println("sum is " + sum);
+}
+}
+
