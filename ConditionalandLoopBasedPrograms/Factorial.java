962b5f0ea3abdaa0f54ff027870de6a975dc308c
Tejaswicsd <tejaswiguttula1@gmail.com>
2026-01-11 22:57:31 +0530

    change factorial variable type from long to int for consistency

--- ConditionalandLoopBasedPrograms/Factorial.java
+++ ConditionalandLoopBasedPrograms/Factorial.java
@@ -8,7 +8,7 @@ public class Factorial {
         System.out.print("Enter a number: ");
         int n = sc.nextInt();
 
-        long fact = 1;
+        int fact = 1;
 
         for (int i = 1; i <= n; i++) {
             fact = fact * i;
